179cdee8d199ccdd9fa675ebfe0631690537cde0
Harsh-Bishnoi <harshbishnoi855@gmail.com>
2025-04-16 10:12:44 +0530

    some changes

--- js/question.js
+++ js/question.js
@@ -30,7 +30,7 @@ Palindrome("madam");
 function factorial(n) {
     let result = 1;
     for (let i = 1; i <= n; i++) {
-        result *= i;
+        result = result * i;
     }
     console.log("Factorial of " + n + " is " + result);
 }
